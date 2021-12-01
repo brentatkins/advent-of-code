34b452286e593c9cd55b8b9e97de8f49ed43a684
Brent Atkins <brentatkins@gmail.com>
2021-12-01 08:33:17 +0100

    puzzle 2

--- 2021/day1/index.test.ts
+++ 2021/day1/index.test.ts
@@ -1,8 +1,8 @@
-import { countMeasurementsLarger } from ".";
+import { countMeasurementsLarger, createSlidingWindows } from ".";
 import { input } from "../input/day1";
 
 describe("day 1", () => {
-  it("should match sample", () => {
+  it("should match sample for puzzle 1", () => {
     const sampleInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
     const result = countMeasurementsLarger(sampleInput);
     expect(result).toBe(7);
@@ -12,4 +12,18 @@ describe("day 1", () => {
     const result = countMeasurementsLarger(input);
     expect(result).toBe(1226);
   });
+
+  it("should match sample for puzzle 1", () => {
+    const sampleInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    const slidingWindowsOf3 = createSlidingWindows(sampleInput, 3);
+    const result = countMeasurementsLarger(slidingWindowsOf3);
+    expect(result).toBe(5);
+  });
+
+  it("puzzle 2 result", () => {
+    const slidingWindowsOf3 = createSlidingWindows(input, 3);
+    const result = countMeasurementsLarger(slidingWindowsOf3);
+    expect(result).toBe(1252);
+  });
+
 });
--- 2021/day1/index.ts
+++ 2021/day1/index.ts
@@ -3,3 +3,10 @@ export const countMeasurementsLarger = (data: number[]) =>
     (acc, inc, index, source) => (inc > source[index - 1] ? acc + 1 : acc),
     0
   );
+
+export const createSlidingWindows = (data: number[], _size: number) => data.reduce<number[]>((acc, inc, index, source) => {
+  if (index > 1) {
+    return  [...acc, inc + source[index - 1] + source[index - 2]]
+  }
+  return acc;
+}, []);
