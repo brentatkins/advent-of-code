7ca61b9bc1ec7ec858c23278a6f6f2e97c55c92d
Brent Atkins <brentatkins@gmail.com>
2021-12-07 19:20:50 +0100

    day 6

---
+++ 2021/day6/index.test.ts
@@ -0,0 +1,29 @@
+import {countLanternFish} from ".";
+import {input} from "../input/day6";
+
+describe("day 6", () => {
+    it("should match sample for puzzle 1", () => {
+        const sampleInput = [3, 4, 3, 1, 2];
+
+        const result = countLanternFish(sampleInput, 80);
+        expect(result).toBe(5934);
+    });
+
+    it("puzzle 1", () => {
+
+        const result = countLanternFish(input, 80);
+        expect(result).toBe(386640);
+    });
+
+    it("should match sample for puzzle 1", () => {
+        const sampleInput = [3, 4, 3, 1, 2];
+
+        const result = countLanternFish(sampleInput, 256);
+        expect(result).toBe(26984457539);
+    });
+
+    it("puzzle 2", () => {
+        const result = countLanternFish(input, 256);
+        expect(result).toBe(1733403626279);
+    });
+});
---
+++ 2021/day6/index.ts
@@ -0,0 +1,33 @@
+import R from "ramda";
+
+export const countLanternFish = (ages: number[], days: number) => {
+  const agesObject = R.pipe(
+    R.groupBy((x) => `${x}`),
+    R.mapObjIndexed((x) => x.length)
+  )(ages);
+  const aged = timeMachine(agesObject, days);
+  const totalFish = R.pipe(R.values, R.sum)(aged);
+  return totalFish;
+};
+
+const ageLanternFish = (
+  ages: Record<string, number>
+): Record<string, number> => {
+  return {
+    "8": ages["0"] ?? 0,
+    "7": ages["8"] ?? 0,
+    "6": (ages["7"] ?? 0) + (ages["0"] ?? 0),
+    "5": ages["6"] ?? 0,
+    "4": ages["5"] ?? 0,
+    "3": ages["4"] ?? 0,
+    "2": ages["3"] ?? 0,
+    "1": ages["2"] ?? 0,
+    "0": ages["1"] ?? 0,
+  };
+};
+
+const timeMachine = (
+  ages: Record<string, number>,
+  days: number
+): Record<string, number> =>
+  days === 0 ? ages : timeMachine(ageLanternFish(ages), days - 1);
